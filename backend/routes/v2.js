b5134a3d53cdf3094b32d3052807298cea5f08d5
nelsonpereira1991 <nelsonpereira1991@gmail.com>
2018-11-18 23:21:52 +0000

    Working on project structure refactoring

---
+++ backend/routes/v2.js
@@ -0,0 +1,10 @@
+module.exports = function (app, io) {
+
+    app.get("v2/", function (req, res) {
+        let response = {
+            status: 200,
+            message: "This is the v2 landing page!"
+        };
+        res.send(response);
+    });
+}
