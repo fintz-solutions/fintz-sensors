0c8bcc8f047182ef1c77cef94b188c3607acf323
Jorge <jorge@MacBook-Pro-de-Jorge.local>
2019-02-21 20:04:16 +0000

    add session and run number to stats templates

--- src/controllers/stats.js
+++ src/controllers/stats.js
@@ -6,23 +6,33 @@ const responseUtil = require(path.resolve(global.utilsFolder, "response"));
 module.exports = {
     getRunStats: function (req, res) {
 
+        let session = req.session._doc;
+        let run = req.run._doc;
+        let iterations = req.iterations;
+        let events = req.events;
+
         if (req.get("Content-Type") === "application/json") {
-            let data = statsService.getRunStats(req.session._doc, req.run._doc, req.iterations, req.events);
+            let data = statsService.getRunStats(session, run, iterations, events);
             responseUtil.sendSuccessResponse("Run stats retrieved successfully", 200, data, res);
         } else {
-            res.render("pages/runs_stats.html.tpl", {
-                title: "Runs Stats"
+            res.render("pages/run_stats.html.tpl", {
+                title: "Run Stats",
+                session: {number: session.number},
+                run: {number: run.number}
             });
         }
     },
     getSessionStats: function (req, res) {
 
+        let session = req.session._doc;
+
         if (req.get("Content-Type") === "application/json") {
-            let data = statsService.getSessionStats(req.session._doc);
+            let data = statsService.getSessionStats(session);
             responseUtil.sendSuccessResponse("Session Stats retrieved successfully", 200, data, res);
         } else {
             res.render("pages/session_stats.html.tpl", {
-                title: "Session Stats"
+                title: "Session Stats",
+                session: {number: session.number}
             });
         }
     }
