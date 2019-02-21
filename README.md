a3bcf2910e58dc12918c486b346d2f809468c999
marcos <marcos.acoliveira91@gmail.com>
2019-02-21 21:14:56 +0000

    Final version of UI for v1.0.0

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Working around with [MESH](https://meshprj.com/) sensors.
 ## License
 
 Fintz-Sensors is currently licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: './src/static/js/main.js',
     output: {
         path: path.resolve(__dirname, 'src/static'),
